ca8b1ce143338d793471beee72b2f40053a3d0dd
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2021-12-14 11:46:17 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro projeto/Program.cs
@@ -0,0 +1,10 @@
+﻿
+var first = "Hello ";
+var second = "World";
+
+
+Console.WriteLine("====================================");
+Console.WriteLine(first + second);
+Console.WriteLine("Meu primeiro projeto utilizando o C#");
+Console.WriteLine("====================================");
+
