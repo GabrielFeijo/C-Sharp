026bab6124a213a19517dc641c4d114a9156d139
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2021-12-14 22:15:03 -0300

    Primeiro Código

---
+++ Projeto001/Projeto001/Program.cs
@@ -0,0 +1,10 @@
+﻿DateTime data = DateTime.Now;
+
+Console.WriteLine("Informe seu nome: ");
+var nome = Console.ReadLine();
+
+
+
+Console.WriteLine("Olá " + nome);
+Console.WriteLine("Seu nome possui " + nome.Length + " letras") ;
+Console.WriteLine("Data e hora " + data);
