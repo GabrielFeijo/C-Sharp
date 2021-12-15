9c43595bd30cf0d3dd85316f0e46f7e8e3eada96
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2021-12-15 12:10:48 -0300

    Novidades

---
+++ Projeto001/Projeto001/Contas.cs
@@ -0,0 +1,10 @@
+﻿namespace Contas
+{
+    public class Contas
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Projeto001/par-impar/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Informe um valor para saber se é par ou ímpar: ");
+int num = Console.Read();
+
+if (num%2 == 0)
+{
+    Console.WriteLine("Número Par!");
+}
+else
+{
+    Console.WriteLine("Número ímpar!");
+}
